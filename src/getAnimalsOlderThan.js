bed06785bd38bf2ef126a3d001e4c337a3040fce
caiobacode <caioodsf@gmail.com>
2022-07-28 17:47:36 -0300

    req2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(animal, idade) {
+  let trfl = true
+  species.forEach(object => {
+    if (object.name === animal) {
+      object.residents.forEach(obj => {
+        if(obj.age < idade) {
+          trfl = false
+
+        }
+      }) 
+    }
+  })
+  return trfl
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,34 +3,27 @@ const data = require('../data/zoo_data');
 const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
 
 function getSpeciesByIds(id1, id2) {
-  const array = []
+  const array = [];
   if (id2 === undefined) {
     species.forEach(object => {
       if (object.id === id1) {
-        array.push(object)
+        array.push(object);
       }
     })
   }
   else {
     species.forEach(object => {
       if (object.id === id1) {
-        array.push(object)
+        array.push(object);
       }
     })
     species.forEach(object => {
       if (object.id === id2) {
-        array.push(object)
+        array.push(object);
       }
     })
   }
-  return array
+  return array;
 }
-
-console.log(getSpeciesByIds(lionId))
-
-//species.forEach(object => {
-  //console.log(object)
-//})
-
 
 module.exports = getSpeciesByIds;
