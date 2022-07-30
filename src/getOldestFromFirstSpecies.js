d2e2fa378f7127108aabc7a2f97a4c7d164ec348
caiobacode <caioodsf@gmail.com>
2022-07-30 19:44:56 -0300

    req10FACIL

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,30 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  let pessoa = 'a'
+  let obEspecie = 'a'
+  employees.forEach(obj => {
+    if (obj.id === id) {
+      pessoa = obj
+    }
+  })
+  let especie = pessoa.responsibleFor[0]
+  species.forEach(ob => {
+    if (ob.id === especie) {
+      obEspecie = ob
+    }
+  })
+  let final = 0
+  let maior = 0
+  obEspecie.residents.forEach(objet => {
+    if (objet.age > maior) {
+      maior = objet.age
+      final = objet
+    }
+  })
+  let ret = Object.values(final)
+  return ret
 }
 
 module.exports = getOldestFromFirstSpecies;
